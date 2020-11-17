0b2a67d8f8f501da92ca12f29c225e7ae292ad0a
ukchanJung <ukchan@kakao.com>
2020-11-17 19:11:31 +0900

    init

---
+++ android/app/src/main/kotlin/com/example/flutter_app_location_todo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_app_location_todo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/location_todo_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LocationTodo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('LTD'),
+      ),
+      body: Column(
+        children: [
+          Image.asset('images/a_dot_burr.jpeg')
+        ],
+      ),
+    );
+  }
+}
+
+
