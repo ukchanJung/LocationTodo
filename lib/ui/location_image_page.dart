06084c75db3677b501e28f9422079c7162ad7b00
ukchanJung <ukchan@kakao.com>
2020-11-23 12:59:44 +0900

    Scatter Chart구현중 -2

--- lib/ui/location_image_page.dart
+++ lib/ui/location_image_page.dart
@@ -16,7 +16,8 @@ class LocationImage extends StatefulWidget {
 class _LocationImageState extends State<LocationImage> {
   double a = 100;
   double b = 100;
-  PhotoViewController pController ;
+  PhotoViewController pController;
+
   int touchedIndex;
 
   Color greyColor = Colors.grey;
@@ -36,6 +37,7 @@ class _LocationImageState extends State<LocationImage> {
     super.dispose();
     pController.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -59,133 +61,131 @@ class _LocationImageState extends State<LocationImage> {
                       StreamBuilder<PhotoViewControllerValue>(
                           stream: pController.outputStateStream,
                           builder: (BuildContext context, AsyncSnapshot<PhotoViewControllerValue> snapshot) {
-                            if (!snapshot.hasData)
-                              return Container();
-                            return Stack( children: widget.tasks.where((e) => e.x != null).map((e) {
+                            if (!snapshot.hasData) return Container();
+                            return Stack(
+                                children: widget.tasks.where((e) => e.x != null).map((e) {
                               return Positioned(
                                 left: e.x,
                                 top: e.y,
                                 child: Opacity(
                                   opacity: 0.8,
                                   child: Transform.scale(
-                                    scale: (1/snapshot.data.scale)*2,
+                                    scale: (1 / snapshot.data.scale) * 2,
                                     //Task class favorite을 활용한 위치확인 기능 구현
                                     child: e.favorite == true
                                         ? Icon(
-                                      Icons.pin_drop,
-                                      color: Colors.red,
-                                    )
+                                            Icons.pin_drop,
+                                            color: Colors.red,
+                                          )
                                         : Icon(Icons.pin_drop),
                                   ),
                                 ),
                               );
                             }).toList());
-                          }
-                      ),
-                      AspectRatio(
-                        aspectRatio: 421/297,
-                        child: ScatterChart(
-                          ScatterChartData(
-                            scatterSpots: [
-                              ScatterSpot(
-                                14,
-                                14,
-                                color: selectedSpots.contains(0) ? Colors.green : greyColor,
-                              ),
-                              ScatterSpot(
-                                22,
-                                25,
-                                color: selectedSpots.contains(1) ? Colors.yellow : greyColor,
-                                radius: 12,
-                              ),
-                              ScatterSpot(
-                                34,
-                                35,
-                                color: selectedSpots.contains(2) ? Colors.purpleAccent : greyColor,
-                                radius: 8,
-                              ),
-                              ScatterSpot(
-                                48,
-                                46,
-                                color: selectedSpots.contains(3) ? Colors.orange : greyColor,
-                                radius: 20,
-                              ),
-                              ScatterSpot(
-                                55,
-                                57,
-                                color: selectedSpots.contains(4) ? Colors.brown : greyColor,
-                                radius: 14,
-                              ),
-                              ScatterSpot(
-                                67,
-                                62,
-                                color: selectedSpots.contains(5) ? Colors.lightGreenAccent : greyColor,
-                                radius: 18,
-                              ),
-                              ScatterSpot(
-                                73,
-                                72,
-                                color: selectedSpots.contains(6) ? Colors.red : greyColor,
-                                radius: 36,
-                              ),
-                              ScatterSpot(
-                                82,
-                                88,
-                                color: selectedSpots.contains(7) ? Colors.tealAccent : greyColor,
-                                radius: 22,
-                              ),
-                            ],
-                            minX: 0,
-                            minY: 0,
-                            maxX: 421,
-                            maxY: 297,
-                            borderData: FlBorderData(
-                              show: true
-                            ),
-                            gridData: FlGridData(
-                              show: true,
-                              drawHorizontalLine: true,
-                              checkToShowHorizontalLine: (value) => true,
-                              getDrawingHorizontalLine: (value) => FlLine(color: Colors.white.withOpacity(0.1)),
-                              drawVerticalLine: true,
-                              checkToShowVerticalLine: (value) => true,
-                              getDrawingVerticalLine: (value) => FlLine(color: Colors.white.withOpacity(0.1)),
-                            ),
-                            titlesData: FlTitlesData(
-                              show: false,
-                            ),
-                            showingTooltipIndicators: selectedSpots,
-                            scatterTouchData: ScatterTouchData(
-                              enabled: true,
-                              handleBuiltInTouches: false,
-                              touchTooltipData: ScatterTouchTooltipData(
-                                tooltipBgColor: Colors.black,
-                              ),
-                              touchCallback: (ScatterTouchResponse touchResponse) {
-                                if (touchResponse.touchInput is FlPanStart) {
-                                  lastPanStartOnIndex = touchResponse.touchedSpotIndex;
-                                } else if (touchResponse.touchInput is FlPanEnd) {
-                                  final FlPanEnd flPanEnd = touchResponse.touchInput;
-
-                                  if (flPanEnd.velocity.pixelsPerSecond <= const Offset(4, 4)) {
-                                    // Tap happened
-                                    setState(() {
-                                      if (selectedSpots.contains(lastPanStartOnIndex)) {
-                                        selectedSpots.remove(lastPanStartOnIndex);
-                                      } else {
-                                        selectedSpots.add(lastPanStartOnIndex);
-                                      }
-                                    });
-                                  }
-                                }
-                              },
-                          ),
-                        ),
-                      ),
-                      )],
+                          }),
+                      //TODO 차트 검토사항
+                      // AspectRatio(
+                      //   aspectRatio: 421 / 297,
+                      //   child: ScatterChart(
+                      //     ScatterChartData(
+                      //       scatterSpots: [
+                      //         ScatterSpot(
+                      //           14,
+                      //           14,
+                      //           color: selectedSpots.contains(0) ? Colors.green : greyColor,
+                      //         ),
+                      //         ScatterSpot(
+                      //           22,
+                      //           25,
+                      //           color: selectedSpots.contains(1) ? Colors.yellow : greyColor,
+                      //           radius: 12,
+                      //         ),
+                      //         ScatterSpot(
+                      //           34,
+                      //           35,
+                      //           color: selectedSpots.contains(2) ? Colors.purpleAccent : greyColor,
+                      //           radius: 8,
+                      //         ),
+                      //         ScatterSpot(
+                      //           48,
+                      //           46,
+                      //           color: selectedSpots.contains(3) ? Colors.orange : greyColor,
+                      //           radius: 20,
+                      //         ),
+                      //         ScatterSpot(
+                      //           55,
+                      //           57,
+                      //           color: selectedSpots.contains(4) ? Colors.brown : greyColor,
+                      //           radius: 14,
+                      //         ),
+                      //         ScatterSpot(
+                      //           67,
+                      //           62,
+                      //           color: selectedSpots.contains(5) ? Colors.lightGreenAccent : greyColor,
+                      //           radius: 18,
+                      //         ),
+                      //         ScatterSpot(
+                      //           73,
+                      //           72,
+                      //           color: selectedSpots.contains(6) ? Colors.red : greyColor,
+                      //           radius: 36,
+                      //         ),
+                      //         ScatterSpot(
+                      //           82,
+                      //           88,
+                      //           color: selectedSpots.contains(7) ? Colors.tealAccent : greyColor,
+                      //           radius: 22,
+                      //         ),
+                      //       ],
+                      //       minX: 0,
+                      //       minY: 0,
+                      //       maxX: 421,
+                      //       maxY: 297,
+                      //       borderData: FlBorderData(show: true),
+                      //       gridData: FlGridData(
+                      //         show: true,
+                      //         drawHorizontalLine: true,
+                      //         checkToShowHorizontalLine: (value) => true,
+                      //         getDrawingHorizontalLine: (value) => FlLine(color: Colors.white.withOpacity(0.1)),
+                      //         drawVerticalLine: true,
+                      //         checkToShowVerticalLine: (value) => true,
+                      //         getDrawingVerticalLine: (value) => FlLine(color: Colors.white.withOpacity(0.1)),
+                      //       ),
+                      //       titlesData: FlTitlesData(
+                      //         show: false,
+                      //       ),
+                      //       showingTooltipIndicators: selectedSpots,
+                      //       scatterTouchData: ScatterTouchData(
+                      //         enabled: true,
+                      //         handleBuiltInTouches: false,
+                      //         touchTooltipData: ScatterTouchTooltipData(
+                      //           tooltipBgColor: Colors.black,
+                      //         ),
+                      //         touchCallback: (ScatterTouchResponse touchResponse) {
+                      //           if (touchResponse.touchInput is FlPanStart) {
+                      //             lastPanStartOnIndex = touchResponse.touchedSpotIndex;
+                      //           } else if (touchResponse.touchInput is FlPanEnd) {
+                      //             final FlPanEnd flPanEnd = touchResponse.touchInput;
+                      //
+                      //             if (flPanEnd.velocity.pixelsPerSecond <= const Offset(4, 4)) {
+                      //               // Tap happened
+                      //               setState(() {
+                      //                 if (selectedSpots.contains(lastPanStartOnIndex)) {
+                      //                   selectedSpots.remove(lastPanStartOnIndex);
+                      //                 } else {
+                      //                   selectedSpots.add(lastPanStartOnIndex);
+                      //                 }
+                      //               });
+                      //             }
+                      //           }
+                      //         },
+                      //       ),
+                      //     ),
+                      //   ),
+                      // ),
+                    ],
                   ),
                 ),
-                
               ],
             ),
           ),
